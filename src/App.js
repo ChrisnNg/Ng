5e68f288a44c1071e3f680824014c4763675c207
ChrisnNg <cristopherng@hotmail.com>
2019-11-13 03:09:44 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import pfp from "./assets/pfp.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={pfp} className="App-logo" alt="logo" />
+        <p>Christopher Ng</p>
+        <p>Full Stack Web Developer</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
