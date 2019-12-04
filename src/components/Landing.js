44a88380f0403088622318ecd5f53898b2199e7a
ChrisnNg <cristopherng@hotmail.com>
2019-12-04 13:57:22 -0800

    modal created

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Landing.css";
 import Fade from "react-reveal/Fade";
 import { Button } from "react-bootstrap";
+import MyVerticallyCenteredModal from "./Modal.js";
 
 const Landing = props => {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <>
       <section id="landing">
@@ -15,13 +18,21 @@ const Landing = props => {
             <p>A Full-Stack Web Developer Graduate from Lighthouse Labs</p>
           </Fade>
 
-          <Button type="button" className="btn btn-info landing-btn">
+          <Button
+            type="button"
+            className="btn btn-info landing-btn"
+            onClick={() => setModalShow(true)}
+          >
             Contact Me
           </Button>
           <Button type="button" className="btn btn-info landing-btn">
             Resume
           </Button>
         </article>
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
       </section>
     </>
   );
